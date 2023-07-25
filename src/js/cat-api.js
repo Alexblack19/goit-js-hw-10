49f50a66e174a404ccac6f3e52fb518f3b9542ad
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-25 20:06:06 +0300

    update

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -2,23 +2,23 @@ const BASE_URL = 'https://api.thecatapi.com';
 const API_KEY =
   'live_9WSo2Jn9CKnphyJArmkanU7amzSO2MnioxrGT7XMimNpxzxlc0kjKpWz3hL08Pnd';
 
-export const fetchBreeds = nameCat =>
-  fetch(`${BASE_URL}/v1/breeds`)
-    .then(response => {
-      console.log(response);
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(console.warn);
+export const fetchBreeds = () =>
+  fetch(`${BASE_URL}/v1/breeds`).then(response => {
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(response);
+    }
+    return response.json();
+  });
 
-fetchBreeds();
+fetchBreeds()
+  .then(cats => cats)
+  .catch(error => {
+    console.log(error);
+  });
 
-
-import axios from "axios";
-
-axios.defaults.headers.common["x-api-key"] = `${API_KEY}`;
+function renderCatCard(cats) {
+  console.log(cats);
+}
+// import axios from "axios";
+// axios.defaults.headers.common["x-api-key"] = `${API_KEY}`;
