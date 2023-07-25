a24b37a75540cbfe9896f1206eca7ee57869adb4
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-25 17:56:48 +0000

    Deploying to gh-pages from @ Alexblack19/goit-js-hw-10@fe8178c42c341447090889b5c8b3d70a481a790c 🚀

--- index.95770348.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((o=>{if(console.log(o),!o.ok)throw new Error(o);return o.json()})).then((o=>o)).catch((o=>{console.log(o)}));
-//# sourceMappingURL=index.95770348.js.map
--- index.9633aa24.js
+++
@@ -1,2 +0,0 @@
-fetch("".concat("https://api.thecatapi.com","/v1/breeds")).then((function(n){if(console.log(n),!n.ok)throw new Error(n);return n.json()})).then((function(n){return n})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.9633aa24.js.map
