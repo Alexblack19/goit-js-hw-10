fe8178c42c341447090889b5c8b3d70a481a790c
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-25 20:56:24 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import fetchBreeds from "./js/cat-api.js"
+import SlimSelect from 'slim-select'
+import axios from "axios";
+import {fetchBreeds} from "./js/cat-api.js"
 
+new SlimSelect({
+    select: '.breed-select'
+  })
+// axios.defaults.headers.common["x-api-key"] = `${API_KEY}`;
+  
+fetchBreeds()
+  .then(renderCatCard)
+  .catch(error => {
+    console.log(error);
+  });
+
+function renderCatCard(cats) {
+    console.log(cats);
+  }
+  
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,24 +1,15 @@
+import axios from 'axios';
+// axios.defaults.headers.common["x-api-key"] = `${API_KEY}`;
+
 const BASE_URL = 'https://api.thecatapi.com';
 const API_KEY =
   'live_9WSo2Jn9CKnphyJArmkanU7amzSO2MnioxrGT7XMimNpxzxlc0kjKpWz3hL08Pnd';
 
 export const fetchBreeds = () =>
-  fetch(`${BASE_URL}/v1/breeds`).then(response => {
+  fetch(`${BASE_URL}/v1/images?api_key=${API_KEY}`).then(response => {
     console.log(response);
     if (!response.ok) {
       throw new Error(response);
     }
     return response.json();
   });
-
-fetchBreeds()
-  .then(cats => cats)
-  .catch(error => {
-    console.log(error);
-  });
-
-function renderCatCard(cats) {
-  console.log(cats);
-}
-// import axios from "axios";
-// axios.defaults.headers.common["x-api-key"] = `${API_KEY}`;
