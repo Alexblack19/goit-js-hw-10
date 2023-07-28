a207e807c15041c5176c74450e255f7c3e8d54bc
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-28 09:50:42 +0000

    Deploying to gh-pages from @ Alexblack19/goit-js-hw-10@c39e75e9dcf20f3d2b191aa28c1417e8c66c5fd3 🚀

--- index.6021fb57.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select");fetch("https://api.thecatapi.com/v1/breeds?api-key=live_9WSo2Jn9CKnphyJArmkanU7amzSO2MnioxrGT7XMimNpxzxlc0kjKpWz3hL08Pnd").then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((function(e){console.log(e)})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.6021fb57.js.map
