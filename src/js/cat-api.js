7156b5639a8a4ad8a9b2f2df1ee2c4161ff59920
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-28 10:18:50 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,16 +1,22 @@
-// import axios from 'axios';
-
 const BASE_URL = 'https://api.thecatapi.com';
-// const API_KEY =
-//   'live_9WSo2Jn9CKnphyJArmkanU7amzSO2MnioxrGT7XMimNpxzxlc0kjKpWz3hL08Pnd';
-  
-// axios.defaults.headers.common['x-api-key'] = `${API_KEY}`;
+const END_POINT_BREEDS = 'v1/breeds';
+const END_POINT_IMAGES = 'v1/images/search';
+
+const API_KEY =
+  'live_9WSo2Jn9CKnphyJArmkanU7amzSO2MnioxrGT7XMimNpxzxlc0kjKpWz3hL08Pnd';
 
 export const fetchBreeds = () =>
-  fetch(`${BASE_URL}/v1/breeds`).then(response => {
-    console.log(response);
+  fetch(`${BASE_URL}/${END_POINT_BREEDS}?api-key=${API_KEY}`).then(response => {
     if (!response.ok) {
-      throw new Error(response);
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+
+export const fetchCatByBreed = breedId =>
+  fetch(`${BASE_URL}/${END_POINT_IMAGES}?api-key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
     return response.json();
   });
