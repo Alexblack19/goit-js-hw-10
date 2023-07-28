c39e75e9dcf20f3d2b191aa28c1417e8c66c5fd3
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-28 12:45:47 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//== Підключення бібліотеки Slim Select ==
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css';
+// =======================================
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api.js';
+
+const selectEl = document.querySelector('.breed-select');
+const divEl = document.querySelector('.cat-info');
+
+fetchBreeds()
+  .then(data => {
+    const markup = data
+      .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+      .join();
+    selectEl.innerHTML = markup;
+    new SlimSelect({
+      select: '.breed-select'
+    })
+  })
+  .catch(error => {
+    console.log(error);
+  });
