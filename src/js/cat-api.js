f9cacb5b6e022353e7003d18b3ea97838b7b888f
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-29 14:37:19 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = 'https://api.thecatapi.com';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_9WSo2Jn9CKnphyJArmkanU7amzSO2MnioxrGT7XMimNpxzxlc0kjKpWz3hL08Pnd';
 
 export const fetchBreeds = () =>
-  fetch(`${BASE_URL}/v1/breeds?api_key=${API_KEY}`).then(response => {
+  fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
@@ -14,7 +14,7 @@ export const fetchBreeds = () =>
 
 export const fetchCatByBreed = breedId =>
   fetch(
-    `${BASE_URL}/v1/images/search?api_key=${API_KEY}&breed_id=${breedId}`
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_id=${breedId}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
